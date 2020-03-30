bf723e2b92835029214e5b2f9fa65cf1cb1f982f
tianjiashuo <249672218@qq.com>
2020-03-30 21:21:13 +0800

    homework

--- WordFrequency/WordFrequency/src/GetFileCharset.java
+++
@@ -1,6 +0,0 @@
-public class GetFileCharset {
-    public static boolean checkUTF(String str) {
-        boolean flag = java.nio.charset.Charset.forName("UTF-8").newEncoder().canEncode(str);
-        return flag;
-    }
-}
